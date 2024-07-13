e704f42a8d5fc19bad778b348bbdba39494c879f
Felföldy Tibor <tibor.felfoldy@bcsconsult.hu>
2024-07-13 15:12:04 +0200

    Update DebugTools package

--- Sources/PythonConsole/PythonConsole.swift
+++ Sources/PythonConsole/PythonConsole.swift
@@ -9,17 +9,7 @@ public enum PythonConsole {
         #if canImport(UIKit)
         DebugTools.shakePresentedConsole = {
             let consoleView = UIHostingController(rootView: PythonConsoleView())
-            
-            if let sheet = consoleView.sheetPresentationController {
-                sheet.detents = [.custom(resolver: { _ in 44 }), .medium(), .large()]
-                sheet.largestUndimmedDetentIdentifier = .medium
-                sheet.prefersScrollingExpandsWhenScrolledToEdge = false
-                sheet.prefersEdgeAttachedInCompactHeight = true
-                sheet.widthFollowsPreferredContentSizeWhenEdgeAttached = true
-                sheet.prefersGrabberVisible = true
-            }
-            
-            return consoleView
+            return ConsoleViewController(base: consoleView)
         }
         #endif
     }
